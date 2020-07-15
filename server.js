fa0d4a644fbb83921a1af40e196d4bf063ad1bd9
Glitch (hello-express) <none>
2020-07-15 13:21:31 +0000

    🛩🖖 Checkpoint ./server.js:18828285/1536 ./shortenUrl.js:18828285/2078 ./cartList.js:18828285/14

--- server.js
+++ server.js
@@ -12,10 +12,12 @@ function in the current file and by writing require(somePath)(app)
 you are  automatically executing that function which implements the endpoint
 route, with the current app variable, which is express()*/
 require('./timeStamp')(app);
-require('/shortenUrl.js')(app);
-//
 //set bodyparser to parse body for any request with a body with content type json
 var jsonParser = bodyParser.json();
+require('./shorten')(app, jsonParser);
+//------------------------------------------------------------------//
+
+
 //this is to let external access to the server
 app.use(cors({optionSuccessStatus: 200})); 
 //----------------------------------------------------
@@ -31,35 +33,8 @@ app.get("/", (request, response) => {
 
 
 
-// URL shortner microservice 
-app.post("/api/shorturl/new", jsonParser, (req, res, next) => { 
-  
-  var url = req.body.url;
-  url = /^http:\/\//.test(url) ? url : `http://${url}`;
-  if (validateURL(url)) {
-    var hash = hashCode(url);
-    res.json({hash: hash})
-    app.get(`/api/shorturl/${hash}`, (req,res,next) => res.redirect(url)) 
-  } else
-    res.send({error: "URL not valid, must be in the form of http(s)://www.myUrl.myDomain"})
-   
-  
-  
-})
-//---------------------------------------------------------------
-//Create a unique URL hash using crypto builtin Nodejs library
-const hashCode = function(str){
-  return str.split("").reduce(function(a,b){a=((a<<5)-a)+b.charCodeAt(0);return a&a},0).toString().split("").slice(1,4).join("")              
-}
-//Validate URL format
 
-const validateURL = function (str){
-  const urlRegExp = new RegExp(/http:\/\/www\.[A-Z0-9a-z.-]+\.\w+$/);
-  return urlRegExp.test(str)
-}
-//---------------------------------------------------------------
-//convert ip network info to readable IP address
-const ipFormat = (str) => str.split(",").slice(0,1).join()
+
 
 
 
---
+++ shorten.js
@@ -0,0 +1,27 @@
+module.exports = function (app, parser){
+  
+  // URL shortner microservice 
+app.post("/api/shorturl/new", parser, (req, res, next) => { 
+  
+  var url = req.body.url;
+  url = /^http:\/\//.test(url) ? url : `http://${url}`;
+  if (validateURL(url)) {
+    var hash = hashCode(url);
+    res.json({hash: hash})
+    app.get(`/api/shorturl/${hash}`, (req,res,next) => res.redirect(url)) 
+  } else
+    res.send({error: "URL not valid, must be in the form of http(s)://www.myUrl.myDomain"})
+   })
+}
+
+
+//Returns an unique hash code by a string
+const hashCode = function(str){
+  return str.split("").reduce(function(a,b){a=((a<<5)-a)+b.charCodeAt(0);return a&a},0).toString().split("").slice(1,4).join("")              
+}
+
+//Validates URL format
+const validateURL = function (str){
+  const urlRegExp = new RegExp(/http:\/\/www\.[A-Z0-9a-z.-]+\.\w+$/);
+  return urlRegExp.test(str)
+}
