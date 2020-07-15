5f26b3da22a6adc97b79378154481cc8d9be553f
Glitch (hello-express) <none>
2020-07-15 12:11:15 +0000

    👕🐀 Checkpoint ./views/index.html:18828285/1500 ./public/script.js:18828285/238

--- public/script.js
+++ public/script.js
@@ -1,9 +1,12 @@
 var URL ="";
-var submitBtn = document.getElementById("submit");
-var input = document.getElementById("input");
-var result = document.getElementById("result");
+var submitBtnS = document.getElementById("submit-shorten");
+var inputS = document.getElementById("input-shorten");
+var resultS = document.getElementById("result-shorten");
+var submitBtnW = document.getElementById("submit-whoami");
+var inputW = document.getElementById("input-whoami");
+var resultW = document.getElementById("result-whoami");
 //-----------------------------------------------------------------------------
-submitBtn.addEventListener('click', ()=>{URL = input.value; fetchNewURL(URL) });
+submitBtnS.addEventListener('click', ()=>{URL = inputS.value; fetchNewURL(URL) });
 //-----------------------------------------------------------------------------
 const fetchNewURL = function (url){ 
   fetch("/api/shorturl/new", {
@@ -16,5 +19,7 @@ const fetchNewURL = function (url){
                               body: JSON.stringify({url: url}) // body data type must match "Content-Type" header //It has to be stringified otherwise it can't be body parsed
                             }
          ).then( res => res.json())
-          .then( data => result.innerHTML = data.error ? data.error : `<a href='https://kais3r-ms.glitch.me/api/shorturl/${data.hash}'>https://kais3r-ms.glitch.me/api/shorturl/${data.hash}</a>`)
+          .then( data => resultS.innerHTML = data.error ? data.error : `<a href='https://kais3r-ms.glitch.me/api/shorturl/${data.hash}'>https://kais3r-ms.glitch.me/api/shorturl/${data.hash}</a>`)
 }
+
+const fetchIP = function ()
