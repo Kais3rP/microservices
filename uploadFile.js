7f8f5c641f2682d6bb4ee1d7d135c28be12052fc
Glitch (hello-express) <none>
2020-07-16 17:59:09 +0000

    👔⚓️ Checkpoint ./uploadFile.js:18828285/334

--- uploadFile.js
+++ uploadFile.js
@@ -1,11 +1,15 @@
 module.exports = function(app){
   
   const multer = require('multer'); //multer is a middleware that parses bodies of POST with multi-part/formdata
+  const fs = require('fs'); //needed to delete the file uploaded once done
+  const util = require('util');
+  
+  
   const upload = multer({dest: 'uploads/'}) //this sets the destination for the files to parse
   
   app.post('/api/upload', upload.single('upfile'), function(req, res, next){   //single() method of multer parses a single file, the paramater it takes is the name of the file specified in form input
     res.json(req.file)
-    
+    util.promisify(fs.unlink(req.file.path)).then( val => console.log(val) ).catch( err => console.log(err)) //deletes the file once the server responded
     
     
   })
