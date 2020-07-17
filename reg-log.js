a55d408ba54367d8684b3c0db3ffd81aca04c181
Glitch (hello-express) <none>
2020-07-17 22:22:38 +0000

    🎌🌯 Checkpoint ./server.js:18828285/200 ./reg-log.js:18828285/145

--- reg-log.js
+++ reg-log.js
@@ -1,11 +1,14 @@
 module.exports = function submitUser (app, parser, mongoose){
   
-  app.post('/api/register', register)
+  let userSchema = new mongoose.Schema({user: String, password: String});
+  let UserModel = mongoose.model('')
+  
+  app.post('/api/register', parser, register)
 }
 
 function register(req, res, next){
   
-  console.log(req)
+  console.log(req.body)
   
   
 }
