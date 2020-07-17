228c61e51dd01c94c178ca09d24aec131d95c7ed
Glitch (hello-express) <none>
2020-07-17 18:40:03 +0000

    🌴👆 Checkpoint ./views/index.html:18828285/517 ./server.js:18828285/13 ./reg-log.js:18828285/10

--- reg-log.js
+++ reg-log.js
@@ -1,4 +1,4 @@
 module.exports = function submitUser (app, parser, mongoose){
   
-  
+  app.post()
 }
--- server.js
+++ server.js
@@ -42,7 +42,7 @@ require('./shorten')(app, jsonParser, mongoose);
 require('./whoami')(app); 
 require('./uploadFile')(app);
 require('./cartList')(app);
-require('./submit')(app, urlParser, mongoose);
+require('./reg-log')(app, urlParser, mongoose);
 
 
 
