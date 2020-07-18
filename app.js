26646b85748d5585f559fb8a7a4e3a634454d45f
Glitch (hello-express) <none>
2020-07-18 16:50:41 +0000

    🐝🐬 Checkpoint ./server.js:18828285/1725 ./app.js:18828285/1701 ./package.json:18828285/45

---
+++ app.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser'); //to parse body of POST methods encoded
+const mongoose = require('mongoose');
+const jsonParser = bodyParser.json();
+const urlParser = bodyParser.urlencoded({extended: false});
+
+//------------------------------------------------------------//
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+
+//------------------------------------------------------------//
+
+
+//this is to let external access to the server
+app.use(cors({optionSuccessStatus: 200})); 
+//----------------------------------------------------
+// make all the files in 'public' available
+// https://expressjs.com/en/starter/static-files.html
+app.use(express.static("public"));
+//--------------------------------------------------------
+
+app.get("/", (request, response) => {
+  response.sendFile(__dirname + "/views/index.html");
+});
+//-------------------------------------------------------------//
+
+
+/*This is how you separate modules for every microservice route, 
+you export it as a function module, it takes app (express()) and other params, and then by requiring it it automatically imports the
+function in the current file and by writing require(somePath)(app)
+you are  automatically executing that function which implements the endpoint
+route, with the current app variable, which is express()*/
+
+
+
+
+//-------------------
+require('./timeStamp')(app);
+require('./shorten')(app, jsonParser, mongoose);
+require('./whoami')(app); 
+require('./uploadFile')(app);
+require('./cartList')(app);
+require('./reg-log')(app, jsonParser, mongoose);
+
+module.exports = app;
