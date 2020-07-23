4211c22f35b4ba95740b5b37e2b6f7bfdcf39b76
Glitch (hello-express) <none>
2020-07-23 17:43:32 +0000

    🐸🍧 Checkpoint ./services/exercises/exercises.js:18828285/30 ./services/exercises/User.js:18828285/53

--- services/exercises/User.js
+++ services/exercises/User.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 //-------------MongoDB Model---------------
-  var userSchema = new mongoose.Schema({username: String, exercises: [{description: String, duration: Number, date: String}] });
+  var userSchema = new mongoose.Schema({username: String, description: String, duration: Number, date: String, exercises: [{description: String, duration: Number, date: String}] });
   var User = mongoose.model('UserEx', userSchema)
  //----------------------------------------------------- 
   
