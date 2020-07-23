41803cdcf1c19e6d733e9e760862527e77c8937f
Glitch (hello-express) <none>
2020-07-23 15:48:25 +0000

    👘👕 Checkpoint ./services/exercises/exercises.js:18828285/327

--- services/exercises/exercises.js
+++ services/exercises/exercises.js
@@ -33,14 +33,14 @@ router.post('/add', urlParser, async function(req,res, next){
   
   try {
         let userDoc = await User.findOne({_id: req.body.id}).exec();
-    console.log(userDoc)
+   
         if (!userDoc) return res.status(400).send({error: "User not found"});
         let userUpdate = await User.update({_id: req.body.id}, {description: req.body.description,
                                                                 duration: req.body.duration,
                                                                 date: validateDate(req.body.date)}); //{duration: req.body.duration}, {date: req.body.date ? req.body.date : new Date()}
        userDoc = await User.findOne({_id: req.body.id}).exec();
-       console.log(userDoc)
-        res.status(200).send(userDoc);
+       console.log({username: userDoc.username, description: userDoc.description, duration: userDoc.duration, id: userDoc._id, date: userDoc.date})
+        res.status(200).send({username: userDoc.username, description: userDoc.description, duration: userDoc.duration, id: userDoc._id, date: userDoc.date});
   } catch {
      res.status(400).send({error: "Something went wrong!"})
   }
